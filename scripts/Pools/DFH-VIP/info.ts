ec5a7457391b39c3759dbc8831d44f1054643402
Khanh Le <khanhle@LMTs-MacBook-Air.local>
2021-11-30 14:12:24 +0700

    Update POOL DFH VIP

--- scripts/Pools/DFH-VIP/info.ts
+++ scripts/Pools/DFH-VIP/info.ts
@@ -3,17 +3,17 @@ module.exports = {
     deploy: {
         title: "DFH for KOL",
         startDate: "2021/11/30 05:00:00 GMT+00:00",
-        endDate: "2021/11/30 15:00:00 GMT+00:00",
+        endDate: "2021/12/02 05:00:00 GMT+00:00",
         minAmountBusd: "1000",
-        maxAmountBusd: "10000",
-        price: "0.04",
+        maxAmountBusd: "7000",
+        price: "0.05",
         raise: "100000",
-        tokenFee: "0"
+        tokenFee: "10"
     },
     upgrade: { 
         /* update after deploy, using for upgrade */
         address: {
-            "testnet": "0xac13d42526f9aef7a2985aaa93729d28d580a4ee",
+            "testnet": "",
             "mainnet": ""            
         }
     }
