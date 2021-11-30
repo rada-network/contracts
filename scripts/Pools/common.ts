4d18d51353b212c82d32e47485d332292753d79b
Khanh Le <khanhle@LMTs-MacBook-Air.local>
2021-11-30 11:18:10 +0700

    Update mainnet token address

--- scripts/Pools/common.ts
+++ scripts/Pools/common.ts
@@ -4,7 +4,7 @@ module.exports = {
         busdAddress: "0x6945239350AE805b0823cB292a4dA5974d166640",
     },
     mainnet: {
-        rirAddress: "0x6768BDC5d03A87942cE7cB143fA74e0DadE0371b",
-        busdAddress: "0x6945239350AE805b0823cB292a4dA5974d166640",
+        rirAddress: "0x30FB969AD2BFCf0f3136362cccC0bCB99a7193bC",
+        busdAddress: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
     }
 };
