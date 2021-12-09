c7d03366ee5a490e7ebfcfa1b64883f229591e62
Khanh Le <khanhlh@gmail.com>
2021-12-09 14:27:57 +0700

    Update Whitelist contract

--- contracts/WhitelistPools.sol
+++ contracts/WhitelistPools.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
-contract WhitelistOnly is
+contract WhitelistPools is
     Initializable,
     ContextUpgradeable,
     PausableUpgradeable,
@@ -36,17 +36,6 @@ contract WhitelistOnly is
         uint256 timestamp
     );
 
-    /* Admins List, state for user */
-    mapping(address => bool) public admins;
-    mapping(address => bool) public approvers;
-    bool claimable; // global state for all,
-
-    ERC20 busdToken;
-    address private WITHDRAW_ADDRESS; /* Address to cashout */
-    address private newWithdrawAddress;
-    uint8 private newWithdrawAddressApprovedCount; 
-    uint8 private adminCount;
-    uint8 private approverCount;
 
     function initialize(
         /* address _tokenAddress, */ // Will setup later, not available at the Pool start
@@ -56,11 +45,73 @@ contract WhitelistOnly is
         __Pausable_init();
 
         busdToken = ERC20(_bUSDAddress);
+
+        setApprover(owner());
     }
 
+
+
+    /**
+        DATA Structure
+     */
+    struct POOL {
+        ERC20 token;
+        uint256 allocationBusd; // total allocation in $
+        uint256 price;
+        uint256 depositedToken; // total deposited in Token
+        uint256 startDate;
+        uint256 endDate;
+
+        uint256 paidAmount; // for paid
+
+        bool claimOnly; // for
+        bool locked; // if locked, cannot change pool info
+    }
+
+    POOL[] public pools;
+
+    struct Investor {
+        uint256 amountBusd; // approved amount in $
+        uint256 allocationBusd; // approved amount in $
+        uint256 claimedToken; // amount of Token claimed
+        bool paid; // mark as paid
+        bool approved; // state to make sure this investor is approved
+    }
+
+    struct ChangeData {
+        address WITHDRAW_ADDRESS;
+        uint256 poolAllocationBusd;
+        uint256 poolEndDate;
+        address tokenAddress;
+        uint128 poolIndex;
+        uint128 approvalCount;
+        mapping (address => bool) approvers;
+    }
+
+    ChangeData private requestChangeData;
+
+
+    ERC20 busdToken;
+    address private WITHDRAW_ADDRESS; /* Address to cashout */
+
+    /* Admins List, state for user */
+    mapping(address => bool) public admins;
+    mapping(address => bool) public approvers;
+    bool claimable; // global state for all,
+
+    uint8 private adminCount;
+    uint8 private approverCount;
+
+
+    mapping(uint256 => mapping(address => Investor)) private investors;
+    mapping(uint256 => address[]) private investorsAddress;
+
+    /**
+        Modifiers
+     */
     // Modifiers
     modifier onlyModerator() {
-        require(msg.sender == owner() || admins[msg.sender] == true || approvers[msg.sender] == true, "Caller is not an admin");
+        require(admins[msg.sender] == true || approvers[msg.sender] == true, "Caller is not an admin");
         _;
     }
     modifier onlyAdmin() {
@@ -68,7 +119,7 @@ contract WhitelistOnly is
         _;
     }
     modifier onlyApprover() {
-        require(msg.sender == owner() || approvers[msg.sender] == true, "Caller is not an approver");
+        require(approvers[msg.sender] == true, "Caller is not an approver");
         _;
     }
     modifier isClaimable() {
@@ -108,57 +159,78 @@ contract WhitelistOnly is
 
 
     //// FOR CASHOUT ADDRESS
-    function requestChangeWithdrawAddress(address _address) external virtual onlyAdmin {
-        require(WITHDRAW_ADDRESS != _address, "Same as current");
-        newWithdrawAddress = _address;
-        newWithdrawAddressApprovedCount = 0;
+    /* Reject change */
+    function rejectRequestChange() external virtual onlyModerator {
+        delete requestChangeData;
     }
 
-    function approveNewWithdrawAddress() external virtual onlyModerator {
-        require(newWithdrawAddress != address(0), "Same as current");
-        newWithdrawAddressApprovedCount++;
+    function approveRequestChange() external virtual onlyApprover {
+        // make sure not duplicate approve
+        require(!requestChangeData.approvers[msg.sender], "Approve already");
+        require(requestChangeData.WITHDRAW_ADDRESS != address(0) || requestChangeData.poolAllocationBusd > 0 || requestChangeData.poolEndDate > 0, "No Data");
+        // make approved
+        requestChangeData.approvalCount++;
+        requestChangeData.approvers[msg.sender] = true;
 
-        if (newWithdrawAddressApprovedCount >= adminCount + approverCount) {
+        if (requestChangeData.approvalCount >= approverCount) {
+            // update change data
+            if (requestChangeData.WITHDRAW_ADDRESS != address(0)) {
+                WITHDRAW_ADDRESS = requestChangeData.WITHDRAW_ADDRESS;
+            }
+
             // apply change when enough agreement
-            WITHDRAW_ADDRESS = newWithdrawAddress;
-            newWithdrawAddressApprovedCount = 0;
+            if (requestChangeData.poolAllocationBusd > 0) {
+                // make sure cannot under approved allocation
+                uint _poolIdx = requestChangeData.poolIndex;
+                uint256 _totalApprovedAllocationBusd;
+                for (uint256 i; i < investorsAddress[_poolIdx].length; i++) {
+                    address _address = investorsAddress[_poolIdx][i];
+                    Investor memory investor = investors[_poolIdx][_address];
+                    if (investor.approved) {
+                        _totalApprovedAllocationBusd += investor.allocationBusd;
+                    }
+                }
+                require(_totalApprovedAllocationBusd <= requestChangeData.poolAllocationBusd, "Invalid Allocation");
+                pools[requestChangeData.poolIndex].allocationBusd = requestChangeData.poolAllocationBusd;
+            }
+
+            if (requestChangeData.poolEndDate > 0) {
+                pools[requestChangeData.poolIndex].endDate = requestChangeData.poolEndDate;
+            }
+
+            if (requestChangeData.tokenAddress != address(0)) {
+                pools[requestChangeData.poolIndex].token = ERC20(requestChangeData.tokenAddress);
+            }
+            // clear change data
+            delete requestChangeData;
+        }
+    }
+
+    function requestChangeWithdrawAddress(address _address) external virtual onlyAdmin {
+        require(WITHDRAW_ADDRESS != _address, "Same as current");
+        delete requestChangeData;
+        requestChangeData.WITHDRAW_ADDRESS = _address;
+    }
+
+    function requestChangePoolData(uint128 _poolIdx, uint256 _allocationBusd, uint256 _endDate, address _tokenAddress) external virtual onlyAdmin {
+        require(_poolIdx < pools.length, "Pool not available");
+        // pool is not locked, then can update directly
+        require(pools[_poolIdx].locked, "Use update pool function please.");
+        require(_allocationBusd > 0 || _endDate > 0, "No Data");
+        delete requestChangeData;
+        requestChangeData.poolIndex = _poolIdx;
+        if (_allocationBusd > 0) {
+            requestChangeData.poolAllocationBusd = _allocationBusd;
+        }
+        if (_endDate > 0) {
+            requestChangeData.poolEndDate = _endDate;
+        }
+        if (_tokenAddress != address(0)) {
+            requestChangeData.tokenAddress = _tokenAddress;
         }
     }
 
 
-
-    /**
-        DATA Structure
-     */
-    struct POOL {
-        ERC20 token;
-        uint256 allocationBusd; // total allocation in $
-        uint256 price;
-        uint256 depositedToken; // total deposited in Token
-        uint256 startDate;
-        uint256 endDate;
-
-        uint256 paidAmount; // for paid
-
-        bool locked; // if locked, cannot change pool info
-    }
-
-    POOL[] public pools;
-
-    struct Investor {
-        uint256 amountBusd; // approved amount in $
-        uint256 allocationBusd; // approved amount in $
-        uint256 claimedToken; // amount of Token claimed
-        bool paid; // mark as paid
-        bool approved; // state to make sure this investor is approved
-    }
-
-    mapping(uint256 => mapping(address => Investor)) private investors;
-    mapping(uint256 => address[]) private investorsAddress;
-
-    /**
-        Modifiers
-     */
 
     /**
         GETTER
@@ -180,7 +252,7 @@ contract WhitelistOnly is
      */
 
     // Add/update/delete Pool - by Admin
-    function addPool(
+    function addClaimOnlyPool(
         address _tokenAddress,
         uint256 _allocationBusd,
         uint256 _price
@@ -192,12 +264,35 @@ contract WhitelistOnly is
         pool.token = ERC20(_tokenAddress);
         pool.allocationBusd = _allocationBusd;
         pool.price = _price;
+        pool.claimOnly = true;
+
+        pools.push(pool);
+    }
+
+    function addPayablePool(
+        uint256 _allocationBusd,
+        uint256 _price,
+        uint256 _startDate,
+        uint256 _endDate
+    ) public virtual onlyAdmin {
+        require(_allocationBusd > 0, "Invalid allocationBusd");
+        require(_price > 0, "Invalid Price");
+
+        POOL memory pool;
+        pool.allocationBusd = _allocationBusd;
+        pool.price = _price;
+        pool.startDate = _startDate;
+        pool.endDate = _endDate;
+
+        pools.push(pool);
     }
 
     function updatePool(
         uint256 _poolIdx,
         uint256 _allocationBusd,
-        uint256 _price
+        uint256 _price,
+        uint256 _startDate,
+        uint256 _endDate
     ) public virtual onlyAdmin {
         require(_allocationBusd > 0, "Invalid allocationBusd");
         require(_price > 0, "Invalid Price");
@@ -208,8 +303,10 @@ contract WhitelistOnly is
         require(!pool.locked, "Pool locked");
 
         // do update
-        pools[_poolIdx].allocationBusd = _allocationBusd;
-        pools[_poolIdx].price = _price;
+        if (_allocationBusd > 0) pools[_poolIdx].allocationBusd = _allocationBusd;
+        if (_price > 0) pools[_poolIdx].price = _price;
+        if (_startDate > 0) pools[_poolIdx].startDate = _startDate;
+        if (_endDate > 0) pools[_poolIdx].endDate = _endDate;
     }
 
     // Lock / unlock pool - By Approver
@@ -234,14 +331,16 @@ contract WhitelistOnly is
         pools[_poolIdx].locked = false;
     }
 
+    
     // Add / Import Investor
     function importInvestor(
         uint256 _poolIdx,
         address[] memory _addresses,
-        uint256[] memory _amounts
+        uint256[] memory _amountBusds,
+        uint256[] memory _allocationBusds
     ) public virtual onlyAdmin {
         require(_poolIdx < pools.length, "Pool not available");
-        require(_addresses.length == _amounts.length, "Length not match");
+        require(_addresses.length == _amountBusds.length, "Length not match");
 
         POOL memory pool = pools[_poolIdx]; // pool info
 
@@ -249,7 +348,11 @@ contract WhitelistOnly is
             Investor memory investor = investors[_poolIdx][_addresses[i]];
             require(!investor.approved, "User is already approved");
             require(
-                investor.claimedToken.mul(pool.price) <= _amounts[i],
+                investor.claimedToken.mul(pool.price) <= _amountBusds[i],
+                "Invalid Amount"
+            );
+            require(
+                _allocationBusds[i] <= _amountBusds[i],
                 "Invalid Amount"
             );
         }
@@ -261,9 +364,13 @@ contract WhitelistOnly is
             if (investors[_poolIdx][_address].allocationBusd == 0) {
                 investorsAddress[_poolIdx].push(_address);
             }
-            uint256 _amount = _amounts[i];
-            if (_amount == 0) _amount = 1; // using a tiny value, will not valid to claim
-            investors[_poolIdx][_address].allocationBusd = _amount;
+
+            // update amount & allocation
+            investors[_poolIdx][_address].amountBusd = _amountBusds[i];
+
+            uint256 _allocationBusd = _allocationBusds[i];
+            if (_allocationBusd == 0) _allocationBusd = 1; // using a tiny value, will not valid to claim
+            investors[_poolIdx][_address].allocationBusd = _allocationBusd;
         }
     }
 
@@ -343,11 +450,13 @@ contract WhitelistOnly is
         address _address = msg.sender;
         Investor memory investor = investors[_poolIdx][_address];
         
-        if (!investor.paid) return 0; // require paid
+        if (!investor.approved) return 0; // require approved
         
         if (_poolIdx < pools.length) return 0;
         POOL memory pool = pools[_poolIdx]; // pool info
         if (!pool.locked) return 0;
+
+        if (!pool.claimOnly && !investor.paid) return 0; // require paid
 
         uint256 _deposited = pool.depositedToken;
 
@@ -363,12 +472,6 @@ contract WhitelistOnly is
 
         POOL memory pool = pools[_poolIdx]; // pool info
 
-        // make sure not out of max
-        // require(
-        //     getTotalTokenForWinner(msg.sender) >=
-        //         subscription[msg.sender].claimedTokenToken + claimable[1],
-        //     "Cannot claim more token than approved"
-        // );
         // available claim busd
         require(
             pool.token.balanceOf(address(this)) >= _claimable,
@@ -400,6 +503,7 @@ contract WhitelistOnly is
         // check pool
         require(_poolIdx < pools.length, "Pool not available");
         POOL memory pool = pools[_poolIdx]; // pool info
+        require(!pool.claimOnly, "Not require payment");
         require(pool.locked, "Pool not active");
 
         // require project is open and not expire
